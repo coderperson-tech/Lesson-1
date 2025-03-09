239c7492791b11ed212faae43af32612a61a978f
coderperson-tech <rich.sam1209@gmail.com>
2025-03-09 16:21:05 +0400

    initial commit

---
+++ Activity1.py
@@ -0,0 +1,15 @@
+num_list = [1,2,3,4,5]
+
+print(num_list)
+print(num_list[2])
+print(num_list[-1])
+
+listing = ["hi",1,False]
+
+listing.append("hello")
+listing.remove(False)
+listing.pop(1)
+listing.sort()
+listing.reverse()
+listing.clear()
+print(listing)
