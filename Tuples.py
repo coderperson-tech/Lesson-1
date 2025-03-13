13762c6489f12fcfa27d799bac7dcea2119dc0de
coderperson-tech <rich.sam1209@gmail.com>
2025-03-13 19:26:44 +0400

    initial commit

---
+++ Tuples.py
@@ -0,0 +1,24 @@
+num_tuple = (10,20,30,40,50)
+print(num_tuple)
+print(num_tuple[1])
+print(num_tuple[-3])
+
+
+str_tuple = ("apple","banana","cherry","elder berry")
+print(str_tuple)
+
+stu_tuple = ("apple",30,34.5,True)
+
+print(stu_tuple)
+print(stu_tuple[0])
+print(stu_tuple[1])
+print(stu_tuple[2])
+
+print(stu_tuple[1:3])
+
+
+nested_tuple = ((1,2,3,4,5),(6,7,8,9,10),(11,12,13,14,15))
+
+print(nested_tuple)
+print(nested_tuple[0])
+print(nested_tuple[1:2])
